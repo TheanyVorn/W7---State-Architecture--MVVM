124138e09ff7f3c7bfe4c752d190464c314e2d95
Theany <vornnaratheany@gmail.com>
2026-03-04 22:29:22 +0700

    update: move UI code

--- ui/screens/library/library_screen.dart
+++ ui/screens/library/library_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
- 
+import 'package:test/State-Architecture-MVVM/ui/screens/library/widgets/library_content.dart';
+
 import '../../../data/repositories/songs/song_repository.dart';
 import '../../../model/songs/song.dart';
 import '../../states/player_state.dart';
@@ -13,64 +14,68 @@ class LibraryScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // 1- Read the globbal song repository
-    SongRepository songRepository = context.read<SongRepository>();
-    List<Song> songs = songRepository.fetchSongs();
+    // SongRepository songRepository = context.read<SongRepository>();
+    // List<Song> songs = songRepository.fetchSongs();
 
     // 2- Read the globbal settings state
-    AppSettingsState settingsState = context.read<AppSettingsState>();
+    //AppSettingsState settingsState = context.read<AppSettingsState>();
 
     // 3 - Watch the globbal player state
-    PlayerState playerState = context.watch<PlayerState>();
+    //PlayerState playerState = context.watch<PlayerState>();
 
-    return Container(
-      color: settingsState.theme.backgroundColor,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          SizedBox(height: 16),
-          Text("Library", style: AppTextStyles.heading),
+    //     return Container(
+    //       color: settingsState.theme.backgroundColor,
+    //       child: Column(
+    //         crossAxisAlignment: CrossAxisAlignment.center,
+    //         children: [
+    //           SizedBox(height: 16),
+    //           Text("Library", style: AppTextStyles.heading),
 
-          SizedBox(height: 50),
+    //           SizedBox(height: 50),
 
-          Expanded(
-            child: ListView.builder(
-              itemCount: songs.length,
-              itemBuilder: (context, index) => SongTile(
-                song: songs[index],
-                isPlaying: playerState.currentSong == songs[index],
-                onTap: () {
-                  playerState.start(songs[index]);
-                },
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
+    //           Expanded(
+    //             child: ListView.builder(
+    //               itemCount: songs.length,
+    //               itemBuilder: (context, index) => SongTile(
+    //                 song: songs[index],
+    //                 isPlaying: playerState.currentSong == songs[index],
+    //                 onTap: () {
+    //                   playerState.start(songs[index]);
+    //                 },
+    //               ),
+    //             ),
+    //           ),
+    //         ],
+    //       ),
+    //     );
+    //   }
+    // }
 
-class SongTile extends StatelessWidget {
-  const SongTile({
-    super.key,
-    required this.song,
-    required this.isPlaying,
-    required this.onTap,
-  });
+    // class SongTile extends StatelessWidget {
+    //   const SongTile({
+    //     super.key,
+    //     required this.song,
+    //     required this.isPlaying,
+    //     required this.onTap,
+    //   });
 
-  final Song song;
-  final bool isPlaying;
-  final VoidCallback onTap;
+    //   final Song song;
+    //   final bool isPlaying;
+    //   final VoidCallback onTap;
 
-  @override
-  Widget build(BuildContext context) {
-    return ListTile(
-      onTap: onTap,
-      title: Text(song.title),
-      trailing: Text(
-        isPlaying ? "Playing" : "",
-        style: TextStyle(color: Colors.amber),
-      ),
-    );
+    //   @override
+    //   Widget build(BuildContext context) {
+    //     return ListTile(
+    //       onTap: onTap,
+    //       title: Text(song.title),
+    //       trailing: Text(
+    //         isPlaying ? "Playing" : "",
+    //         style: TextStyle(color: Colors.amber),
+    //       ),
+    //     );
+    //   }
+    // }
+
+    return const LibraryContent();
   }
 }
